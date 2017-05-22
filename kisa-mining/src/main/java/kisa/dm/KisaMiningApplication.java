0f865137f907cd7e06b07dadf0e2706c310fc086
Aldin <aldin.kiselica.94@gmail.com>
2017-05-22 20:58:46 +0200

    project added to git repo

---
+++ kisa-mining/src/main/java/kisa/dm/KisaMiningApplication.java
@@ -0,0 +1,12 @@
+package kisa.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisaMiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisaMiningApplication.class, args);
+	}
+}
---
+++ kisa-mining/src/test/java/kisa/dm/KisaMiningApplicationTests.java
@@ -0,0 +1,16 @@
+package kisa.dm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KisaMiningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
